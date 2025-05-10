cc13534dfe44ec8d6eb1fd051a238ddba332bd2a
Thiago Barbosa <thiago3510@gmail.com>
2025-05-10 12:45:20 -0300

    feat: add paths and app-level constants

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export const USERS = [
+  {
+    id: "0",
+    email: "convidado@finances.com" as const,
+    password: "bix9001" as const,
+  },
+];
+
+export const LOCAL_STORAGE_KEYS = {
+  USER: "USER",
+};
+
+export const COOKIES_KEYS = {
+  TOAST: "TOAST",
+  USER: "USER",
+};
