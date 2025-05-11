61801871567da7b9823e61ef14458cd70b7b8176
Thiago Barbosa <thiago3510@gmail.com>
2025-05-11 14:54:27 -0300

    feat: add date utils

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,6 @@
+import { format, parseISO } from "date-fns";
+import { DateRange } from "react-day-picker";
+
 /**
  * Formats a date in epoch time to a string format dd/MM/yyyy HH:mm
  * @param date - epoch time in milliseconds
@@ -11,4 +14,27 @@ export const formatTime = (date: number) => {
     hour: "2-digit",
     minute: "2-digit",
   });
+};
+
+/**
+ * Converts URL date range to DateRange object
+ * @param period: format YYYY-MM-DD_YYYY-MM-DD (start_end)
+ */
+export const getDateRangeFromPeriod = (
+  period: string
+): DateRange | undefined => {
+  const [start, end] = period.split("_");
+
+  if (start && end) {
+    const dates = { from: parseISO(start), to: parseISO(end) };
+    return dates;
+  }
+};
+
+/**
+ * Converts Date to YYYY-MM-DD
+ * @param date: Date object
+ */
+export const formatDateToYYYYMMDD = (date: Date) => {
+  return format(date, "yyyy-MM-dd");
 };
