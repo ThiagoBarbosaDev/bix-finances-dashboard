d7796dc0b0caf329a3871f91af617e26c8b718ab
Thiago Barbosa <thiago3510@gmail.com>
2025-05-12 01:08:25 -0300

    fix: build

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -4,6 +4,7 @@ import { Box, Flex, useBreakpointValue } from "@chakra-ui/react";
 import Sidebar from "@/features/sidebar/components/sidebar";
 import { TransactionStateProvider } from "@/features/transaction/contexts/transaction-context";
 import { ChartStateProvider } from "./dashboard/_charts/contexts/chart-context";
+import { Suspense } from "react";
 
 export default function AuthenticatedLayout({
   children,
@@ -13,21 +14,25 @@ export default function AuthenticatedLayout({
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <ChartStateProvider>
-      <Flex width="100%" height="100vh">
-        <Sidebar />
+    <Suspense fallback={<Box>Loading...</Box>}>
+      <TransactionStateProvider>
+        <ChartStateProvider>
+          <Flex width="100%" height="100vh">
+            <Sidebar />
 
-        <Box
-          as="main"
-          ml={isMobile ? "0px" : "50px"}
-          mt={isMobile ? "40px" : "0px"}
-          height="100%"
-          overflowY="auto"
-          flex="1"
-        >
-          <TransactionStateProvider>{children}</TransactionStateProvider>
-        </Box>
-      </Flex>
-    </ChartStateProvider>
+            <Box
+              as="main"
+              ml={isMobile ? "0px" : "50px"}
+              mt={isMobile ? "40px" : "0px"}
+              height="100%"
+              overflowY="auto"
+              flex="1"
+            >
+              {children}
+            </Box>
+          </Flex>
+        </ChartStateProvider>
+      </TransactionStateProvider>
+    </Suspense>
   );
 }
